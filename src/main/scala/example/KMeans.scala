823c0cf3919368533fc88be3a7db5a940018f402
Windymelt <windymelt@3qe.us>
2022-08-20 21:07:09 +0900

    refactor

--- src/main/scala/example/KMeans.scala
+++ src/main/scala/example/KMeans.scala
@@ -3,7 +3,6 @@ package example
 trait KMeans {
   val alpha = 10
   def kmeans(x: Seq[Color], nk: Int): Seq[Color] = {
-    val grouped = x
     var oldK: Seq[Color] = Seq()
     var initialK = Seq.fill(nk)(Util.choosePixel(x))
     var diff = 99999
@@ -12,11 +11,11 @@ trait KMeans {
     while (diff > alpha) {
       // println(s"initialK $initialK")
       // let x belong to ks
-      belongMap = grouped.map { pix =>
+      belongMap = x.map { pix =>
         initialK.minBy(k => Util.colorDelta(k, pix))
       }
       // println(s"gr# ${grouped.size} bl# ${belongMap.size}")
-      val zipped = grouped zip belongMap
+      val zipped = x zip belongMap
       oldK = initialK
       initialK = initialK map { k =>
         val belongingToK = zipped.filter(_._2 == k)
@@ -25,11 +24,11 @@ trait KMeans {
           belongingToK
             .map(pair => Seq(pair._1.r, pair._1.g, pair._1.b, pair._1.a))
             .transpose
-            .map(_.sum / grouped.size)
+            .map(_.sum / x.size)
         )
       }
       // fit initialK to existing pixel
-      initialK = initialK.map(k => grouped.minBy(Util.colorDelta(_, k)))
+      initialK = initialK.map(k => x.minBy(Util.colorDelta(_, k)))
 
       // get delta
       diff = (oldK zip initialK).map { case (c, d) =>
